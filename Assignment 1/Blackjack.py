dca52a6d20b6feb358a9d4a19a3ba199855fc027
ThePenne <pan.eyal@gmail.com>
2024-01-31 17:45:43 +0200

    finished ass1

--- Assignment 1/Blackjack.py
+++ Assignment 1/Blackjack.py
@@ -1,11 +1,10 @@
 import numpy as np
 from tqdm import tqdm
 import gymnasium as gym
-import seaborn as sns
 
 from itertools import product
-from matplotlib import pyplot as plt
-from matplotlib.patches import Patch
+
+# Here we will implement the policy iteration algorithm for the Blackjack environment.
 
 # State representation: Tuple(sum_player, sum_dealer, usable_ace)
 ALL_STATES = list(product(range(4, 22), range(1, 12), range(2)))
@@ -36,13 +35,6 @@ def transition_matrix(sample_size=10 ** 5, should_print=True):
                                             [0. for _ in ALL_STATES_WITH_END]))
                                    for _ in range(len(ALL_ACTIONS))]))
                          for _ in range(len(ALL_STATES_WITH_END))]))
-
-    # r_s_a: Dict(state -> Dict(action -> Counter))
-    r_s_a = dict(zip(ALL_STATES_WITH_END,
-                     [dict(zip(ALL_ACTIONS, [0. for _ in ALL_ACTIONS])) for _ in ALL_STATES]
-                     + [dict(zip(ALL_ACTIONS, [1. for _ in ALL_ACTIONS]))]
-                     + [dict(zip(ALL_ACTIONS, [-1. for _ in ALL_ACTIONS]))]
-                     + [dict(zip(ALL_ACTIONS, [0. for _ in ALL_ACTIONS]))]))
 
     for _ in tqdm(range(sample_size)):
         action = env.action_space.sample()
@@ -83,7 +75,17 @@ def transition_matrix(sample_size=10 ** 5, should_print=True):
                         print(f"s: {state}, a: {action}, s': {next_state}, P: {np.round(p, 3)}")
 
     env.close()
-    return tr_matrix, r_s_a
+    return tr_matrix
+
+
+def reward_function():
+    # r_s_a: Dict(state -> Dict(action -> Counter))
+    r_s_a = dict(zip(ALL_STATES_WITH_END,
+                     [dict(zip(ALL_ACTIONS, [0. for _ in ALL_ACTIONS])) for _ in ALL_STATES]
+                     + [dict(zip(ALL_ACTIONS, [1. for _ in ALL_ACTIONS]))]
+                     + [dict(zip(ALL_ACTIONS, [-1. for _ in ALL_ACTIONS]))]
+                     + [dict(zip(ALL_ACTIONS, [0. for _ in ALL_ACTIONS]))]))
+    return r_s_a
 
 
 def approximate_policy_evaluation(v, pi, tr_matrix, r_s_a, k=5):
@@ -109,7 +111,7 @@ def policy_improvement(v, pi, tr_matrix, r_s_a):
     return pi
 
 
-def policy_iteration(tr_matrix, r_s_a, k=5, v=None, pi=None, states_to_print=None):
+def policy_iteration(tr_matrix, r_s_a, k=5, v=None, pi=None):
     if v is None:
         v = dict(zip(ALL_STATES_WITH_END,
                      [0 for _ in ALL_STATES_WITH_END]))
@@ -126,10 +128,6 @@ def policy_iteration(tr_matrix, r_s_a, k=5, v=None, pi=None, states_to_print=Non
         pi_old = pi.copy()
         pi = policy_improvement(v, pi, tr_matrix, r_s_a)
 
-        if states_to_print is not None:
-            for state, chosen_actions in states_to_print.items():
-                chosen_actions.append(pi[state])
-
         pi_diff = {k: pi[k] for k in pi if k in pi_old and pi[k] != pi_old[k]}
         if len(pi_diff) == 0:
             print(f"Policy iteration converged after {counter} iterations.")
@@ -138,108 +136,6 @@ def policy_iteration(tr_matrix, r_s_a, k=5, v=None, pi=None, states_to_print=Non
             print(f"counter: {counter}, diff in pi: {len(pi_diff)}", end='\r')
 
     return v, pi, values_avg
-
-
-# def create_grids(v, pi, usable_ace=False):
-#     player_count, dealer_count = np.meshgrid(
-#         # players count, dealers face-up card
-#         np.arange(4, 21) if not usable_ace else np.arange(11, 22),
-    #     np.arange(1, 12),
-    # )
-    #
-    # # create the value grid for plotting
-    # value = np.apply_along_axis(
-    #     lambda obs: v[(obs[0], obs[1], usable_ace)],
-    #     axis=2,
-    #     arr=np.dstack([player_count, dealer_count]),
-    # )
-    # value_grid = player_count, dealer_count, value
-    #
-    # # create the policy grid for plotting
-    # policy_grid = np.apply_along_axis(
-    #     lambda obs: pi[(obs[0], obs[1], usable_ace)],
-    #     axis=2,
-    #     arr=np.dstack([player_count, dealer_count]),
-    # )
-    # return value_grid, policy_grid
-
-
-def create_plots(value_grid, policy_grid, title: str):
-    """Creates a plot using a value and policy grid."""
-    # create a new figure with 2 subplots (left: state values, right: policy)
-    player_count, dealer_count, value = value_grid
-    fig = plt.figure(figsize=(15, 7))
-    fig.suptitle(title, fontsize=16)
-
-    # plot the state values
-    ax1 = fig.add_subplot(1, 2, 1, projection="3d")
-    ax1.plot_surface(
-        player_count,
-        dealer_count,
-        value,
-        rstride=1,
-        cstride=1,
-        cmap="viridis",
-        edgecolor="none",
-    )
-    plt.xticks(value_grid[0][1], value_grid[0][1])
-    plt.yticks(range(1, 12), ["A"] + list(range(2, 12)))
-    ax1.set_title(f"State values: {title}")
-    ax1.set_xlabel("Player sum")
-    ax1.set_ylabel("Dealer showing")
-    ax1.zaxis.set_rotate_label(False)
-    ax1.set_zlabel("Value", fontsize=14, rotation=90)
-    ax1.view_init(20, 220)
-
-    # plot the policy
-    fig.add_subplot(1, 2, 2)
-    ax2 = sns.heatmap(policy_grid, linewidth=0, annot=True, cmap="Accent_r", cbar=False)
-    ax2.set_title(f"Policy: {title}")
-    ax2.set_xlabel("Player sum")
-    ax2.set_ylabel("Dealer showing")
-    ax2.set_xticklabels(value_grid[0][1])
-    ax2.set_yticklabels(["A"] + list(range(2, 12)), fontsize=12)
-
-    # add a legend
-    legend_elements = [
-        Patch(facecolor="lightgreen", edgecolor="black", label="Hit"),
-        Patch(facecolor="grey", edgecolor="black", label="Stick"),
-    ]
-    ax2.legend(handles=legend_elements, bbox_to_anchor=(1.3, 1))
-    return fig
-
-
-# # ----------------------------- Main ----------------------------- #
-#
-#
-# # Get transition matrix and reward function
-# print("Generating transition matrix and reward function...")
-# tr_matrix, r_s_a = transition_matrix(sample_size=10 ** 5, should_print=False)
-#
-# pi = dict()
-# for state in ALL_STATES:
-#     pi[state] = 1 if state[0] < 21 else 0
-# for state in [WIN, LOSE, DRAW]:
-#     pi[state] = 0
-#
-# SIZE = 5
-# indices = np.random.choice(np.arange(len(ALL_STATES)), size=SIZE, replace=False)
-# states_indices = [ALL_STATES[i] for i in indices]
-# states_to_print = dict(zip(states_indices, [[] for _ in range(SIZE)]))
-#
-# # Run policy iteration
-# print("Running policy iteration until convergence...")
-# v, pi, value_avg = policy_iteration(tr_matrix, r_s_a, k=5, pi=pi, states_to_print=states_to_print)
-#
-# for state, chosen_actions in states_to_print.items():
-#     plt.plot(np.arange(1, len(states_to_print[state]) + 1), chosen_actions, 'o-',
-#              label=f"state ({state[0]}, {state[1]}, {state[2]})")
-# plt.xlabel("iteration")
-# plt.ylabel("action")
-# plt.yticks([0, 1], ["stick", "hit"])
-# plt.legend()
-# plt.show()
-# plt.clf()
 
 
 def play(pi):
@@ -266,38 +162,4 @@ def play(pi):
 
 def value_function_q3(v, s):
     print(f'For state: ({s[0]}, {s[1]}, {"hit" if s[2] else "stick"}) -> value: {v[s]}')
-#
-#
-# plt.plot(np.arange(len(value_avg)), value_avg, 'o-')
-# plt.title("Average value function over iterations")
-# plt.xlabel("iteration")
-# plt.ylabel("value")
-# plt.show()
-# plt.clf()
-#
-# player_count, dealer_count = np.meshgrid(
-#     # players count, dealers face-up card
-#     np.arange(4, 21),
-#     np.arange(1, 12),
-# )
-# # create the policy grid for plotting
-# pi_grid = np.apply_along_axis(
-#     lambda obs: pi[(obs[0], obs[1], 0)],
-#     axis=2,
-#     arr=np.dstack([player_count, dealer_count]),
-# )
-# # plot the policy for unusable ace
-# plt.figure()
-# sns.heatmap(pi_grid, linewidth=0, annot=True, cmap="Accent_r", cbar=False, xticklabels=range(4, 21), yticklabels=["A"] + list(range(2, 12)))
-# plt.title("Starting policies for unusable ace", fontsize=20)
-# plt.xlabel("Player sum")
-# plt.ylabel("Dealer showing")
-#
-# # add a legend
-# legend_elements = [
-#     Patch(facecolor="lightgreen", edgecolor="black", label="Hit"),
-#     Patch(facecolor="grey", edgecolor="black", label="Stick"),
-# ]
-# plt.legend(handles=legend_elements)
-# plt.show()
-# plt.clf()
+    return v[s]
